ca05ea58dd1804c05f7cb86a554b6effdcdd54ee
Satoru830624 <31788586+Satoru830624@users.noreply.github.com>
2018-03-21 19:55:51 +0900

    add blacklist

---
+++ BlackListInput/BlackListInput/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BlackListInput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.blackListButton = new System.Windows.Forms.Button();
+            this.blandButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ASINまたはブランド名を入力する";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 51);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(254, 131);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // blackListButton
+            // 
+            this.blackListButton.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.blackListButton.Location = new System.Drawing.Point(12, 188);
+            this.blackListButton.Name = "blackListButton";
+            this.blackListButton.Size = new System.Drawing.Size(130, 58);
+            this.blackListButton.TabIndex = 2;
+            this.blackListButton.Text = "ブラックリスト登録";
+            this.blackListButton.UseVisualStyleBackColor = true;
+            this.blackListButton.Click += new System.EventHandler(this.blackListButton_Click);
+            // 
+            // blandButton
+            // 
+            this.blandButton.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.blandButton.Location = new System.Drawing.Point(145, 188);
+            this.blandButton.Name = "blandButton";
+            this.blandButton.Size = new System.Drawing.Size(121, 58);
+            this.blandButton.TabIndex = 3;
+            this.blandButton.Text = "規制ブランド登録";
+            this.blandButton.UseVisualStyleBackColor = true;
+            this.blandButton.Click += new System.EventHandler(this.blandButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(165, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ver1.1(2018.01.25)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.blandButton);
+            this.Controls.Add(this.blackListButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ブラックリスト登録";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button blackListButton;
+        private System.Windows.Forms.Button blandButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BlackListInput/BlackListInput/Form1.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BlackListInput
+{
+    public partial class Form1 : Form
+    {
+        //テキストボックスに入力した値
+        string inputString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void blackListButton_Click(object sender, EventArgs e)
+        {
+            string blackListFilePath = @"C:\Users\sato_\Downloads\2.ブラックリスト\ブラックリスト.xlsx";
+            string blackList2FilePath = @"C:\Users\sato_\Downloads\2.ブラックリスト\BlackList.csv";
+
+            Cursor.Current = Cursors.WaitCursor;
+            var xlApp = new Microsoft.Office.Interop.Excel.Application();
+            //Excelが開かないようにする
+            xlApp.Visible = false;
+            //指定したパスのExcelを起動
+            Workbook wb = xlApp.Workbooks.Open(Filename: blackListFilePath);
+
+            for (int i = 0; i < inputTextBox.Lines.Length; i++)
+            {
+                getInputTextBox(i);
+
+                inputToExcel(xlApp, wb, inputString);
+                
+                InputCsvFile(blackList2FilePath, inputString);
+            }
+
+            wb.Close(true);
+
+            xlApp.Quit();
+
+            Cursor.Current = Cursors.Default;
+
+            //テキストボックスの表示を空にする
+            inputTextBox.Text = "";
+        }
+
+        private void blandButton_Click(object sender, EventArgs e)
+        {
+            string blandFilePath = @"C:\Users\sato_\Downloads\1.出品テンプレートファイル\出品禁止ブランド、キーワード.xlsx";
+            string blandFilePathCsv = @"C:\Users\sato_\Downloads\1.出品テンプレートファイル\出品禁止ブランド、キーワード.csv";
+            Cursor.Current = Cursors.WaitCursor;
+            var xlApp = new Microsoft.Office.Interop.Excel.Application();
+            //Excelが開かないようにする
+            xlApp.Visible = false;
+            //指定したパスのExcelを起動
+            Workbook wb = xlApp.Workbooks.Open(Filename: blandFilePath);
+
+            for (int i = 0; i < inputTextBox.Lines.Length; i++)
+            {
+                getInputTextBox(i);
+
+                inputToExcel(xlApp, wb, inputString);
+
+                InputCsvFile(blandFilePathCsv, inputString);
+            }
+
+            //Appを閉じる
+            wb.Close(true);
+
+            xlApp.Quit();
+
+            Cursor.Current = Cursors.Default;
+
+            //テキストボックスの表示を空にする
+            inputTextBox.Text = "";
+        }
+
+        //テキストボックスの文字列を取得する
+        public void getInputTextBox(int TextLine)
+        {
+            inputString = inputTextBox.Lines[TextLine].Trim();
+        }
+
+        //エクセルにテキストボックスの情報を書き込む
+        public void inputToExcel(Microsoft.Office.Interop.Excel.Application xlApp, Workbook wb, string inputObject)
+        {
+            try
+            {
+                //Sheetを指定
+                ((Worksheet)wb.Sheets[2]).Select();
+            }
+            catch (Exception ex)
+            {
+                //Sheetがなかった場合のエラー処理
+
+                //Appを閉じる
+                wb.Close(false);
+                xlApp.Quit();
+
+                //Errorメッセージ
+                Console.WriteLine("指定したSheetは存在しません．");
+                Console.ReadLine();
+
+                //実行を終了
+                System.Environment.Exit(0);
+            }
+            //変数宣言
+            Range CellRange;
+
+            //最終行を取得
+            var yCount  = wb.Sheets[2].UsedRange.Rows.Count;
+
+            //書き込む場所を指定
+            CellRange = xlApp.Cells[++yCount, 1] as Range;
+
+            //書き込む内容
+            CellRange.Value2 = inputObject;
+        }
+
+        //csvファイルにデータを書き込む
+        public void InputCsvFile(string CsvFilePath, string BlackAsin)
+        {
+            using (var CsvWriteStream = new StreamWriter(CsvFilePath, true, System.Text.Encoding.GetEncoding("sjis")))
+            {
+                CsvWriteStream.WriteLine(BlackAsin);
+            }
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //垂直、水平両方のスクロールバーを表示
+            inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+        }
+    }
+}
